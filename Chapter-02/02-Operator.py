eea570d5653b1fab683c8c9d4852f0546a238886
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-02 02:44:22 +0530

    Add 02-Operator.py with examples of arithmetic, assignment, comparison, and logical operators

---
+++ Chapter-02/02-Operator.py
@@ -0,0 +1,49 @@
+# Arithmetic Operators
+print("Arithmetic Operators:")
+a = 3
+b = 6
+print(b + a)  # Addition
+print(b - a)  # Subtraction
+print(b * a)  # Multiplication
+print(b / a)  # Division
+print(b % a)  # Modulus
+
+# Assignment Operators
+print("Assignment Operators:")
+c = 4
+c += 4  # Equivalent to c = c + 4
+print(c)
+c -= 2  # Equivalent to c = c - 2
+print(c)
+c *= 5  # Equivalent to c = c * 5
+print(c)
+c /= 2  # Equivalent to c = c / 2
+print(c)
+
+# Comparison Operators
+print("Comparison Operators:")
+d = 2 < 4  # Less than
+print(d)
+d = 2 > 4  # Greater than
+print(d)
+d = 2 <= 4  # Less than or equal to
+print(d)
+d = 2 >= 4  # Greater than or equal to
+print(d)
+d = 2 != 4  # Not equal to
+print(d)
+d = 2 == 4  # Equal to
+print(d)
+
+# Logical Operators
+print("Logical Operators:")
+# Truth table of "or"
+print("False or False is", False or False)
+print("True or False is", True or False)
+print("False or True is", False or True)
+print("True or True is", True or True)
+# Truth table of "and"
+print("False and False is", False and False)
+print("True and False is", True and False)
+print("False and True is", False and True)
+print("True and True is", True and True)
