bce8575141e7d1ac4007b206cd3f7c5d9e89a6c2
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-02 02:44:06 +0530

    Add  01-Variable.py

---
+++ Chapter-02/01-Variable.py
@@ -0,0 +1,8 @@
+Integer01 = 10 # Integer variable
+String02 = "AP" # String variable
+Float03 = 3.14 # Float variable
+Boolean04 = False # Boolean variable
+# $noName # Invalid variable name due to $ symbol
+# no@Name # Invalid variable name due to @ symbol
+_noName = "Valid" # Valid variable name with underscore
+no_Name = "Valid" # Valid variable name with underscore
