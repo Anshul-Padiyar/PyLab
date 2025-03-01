a12ede0a503ef6851edad1b41775719ac36fbae0
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-02 02:45:18 +0530

    Add 04-Input.py to demonstrate input handling

---
+++ Chapter-02/04-Input.py
@@ -0,0 +1,13 @@
+num01 = input("Enter a number : ") # Input (string)
+num02 = input("Enter a another number : ") # Input (string)
+
+sum = num01 + num02 # Concatenating
+
+print("Sum of", num01, "and", num02, "is", sum, "\n")
+
+num01 = int(input("Enter a number : ")) # Input (integer)
+num02 = int(input("Enter a another number : ")) # Input (integer)
+
+sum = num01 + num02 # Adding
+
+print("Sum of", num01, "and", num02, "is", sum)
