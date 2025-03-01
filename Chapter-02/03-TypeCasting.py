8919223c5f3905e67f88c575f790e7c54003b7c4
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-02 02:44:35 +0530

    Add 03-TypeCasting.py with examples of type conversion between string, float, and integer

---
+++ Chapter-02/03-TypeCasting.py
@@ -0,0 +1,24 @@
+# Initial string value
+a = "3.14"
+print("a :", a)  # Print the value of 'a'
+print("a :", type(a))  # Output the type of 'a'
+
+# Convert string to float
+b = float(a)
+print("b :", b)  # Print the value of 'b'
+print("b :", type(b))  # Output the type of 'b'
+
+# Convert float to integer
+c = int(b)
+print("c :", c)  # Print the value of 'c'
+print("c :", type(c))  # Output the type of 'c'
+
+# Convert integer to string
+d = str(c)
+print("d :", d)  # Print the value of 'd'
+print("d :", type(d))  # Output the type of 'd'
+
+# Convert string back to integer
+f = int(d)
+print("f :", f)  # Print the value of 'f'
+print("f :", type(f))  # Output the type of 'f'
