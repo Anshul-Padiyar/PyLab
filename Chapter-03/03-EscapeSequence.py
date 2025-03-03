b3fe8554daef8368458d4212541e0ee7c4aed43f
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-04 00:54:39 +0530

    Add Chapter 3 – Escape Sequences

---
+++ Chapter-03/03-EscapeSequence.py
@@ -0,0 +1,17 @@
+string = "Tiny changes,\nremarkable results" # newline 
+print(string)
+
+string = "Tiny changes\t-->\tremarkable results" # tab 
+print(string)
+
+string = "\"Tiny changes, remarkable results\"" # double quote 
+print(string)
+
+string = "'Tiny changes, remarkable results'" # single quote directly
+print(string)
+
+string = '\'Tiny changes, remarkable results\'' # single quote 
+print(string)
+
+string = "Tiny changes \\ remarkable results" # backslash 
+print(string)
