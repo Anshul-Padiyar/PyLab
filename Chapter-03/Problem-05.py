701c54f696ce315e50e4d94467a45483eefe9d97
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-04 01:36:51 +0530

    Add Problem 05: Format letter using escape sequence characters

---
+++ Chapter-03/Problem-05.py
@@ -0,0 +1,4 @@
+# Write a program to format the following letter using escape sequence characters. 
+# letter =  "Hola AP, Hope you’ve completed your español lesson for today. Gracias!"
+
+print("Hola AP,\n\tHope you\'ve completed your español lesson for today.\nGracias!")
