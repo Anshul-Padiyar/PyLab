9e76671a0962c55cb22e3c1ea0b0db85a5afb443
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-06 19:06:19 +0530

    Add Chapter 04: Problem 04 - Sum of a List

---
+++ Chapter-04/Problem-04.py
@@ -0,0 +1,9 @@
+# Write a program to sum a list with 4 numbers.
+
+list01 = [1, 5, 3, 6, 7]
+
+print("The list is:", list01)
+
+total_sum = sum(list01)
+
+print("The sum of the list is:", total_sum)
