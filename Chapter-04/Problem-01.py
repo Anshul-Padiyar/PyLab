0ba09fdde84036700ad47ffe475a7ee573f18224
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-06 18:47:33 +0530

    Add Chapter 04: Problem 01 - Fruit List Program

---
+++ Chapter-04/Problem-01.py
@@ -0,0 +1,13 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+fruits = []
+
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+fruits.append(input("Enter a fruit : "))
+
+print(fruits)
