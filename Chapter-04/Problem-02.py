1dac4046ac8dcf77cc9543b3320d420d3714eaf5
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-06 18:55:17 +0530

    Add Chapter 04: Problem 02 - Student Marks Sorting Program

---
+++ Chapter-04/Problem-02.py
@@ -0,0 +1,16 @@
+# Write a program to accept marks of 6 students and display them in a sorted manner.
+
+marks = []
+
+marks.append(input("Enter student marks: "))
+marks.append(input("Enter student marks: "))
+marks.append(input("Enter student marks: "))
+marks.append(input("Enter student marks: "))
+marks.append(input("Enter student marks: "))
+marks.append(input("Enter student marks: "))
+
+print("Marks before sorting:", marks)
+
+marks.sort()
+
+print("Marks after sorting:", marks)
