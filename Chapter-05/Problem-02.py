0ca326edf9543b8f428df700aef67b420fd529bf
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-19 20:47:47 +0530

    Add Chapter 05: Unique Numbers Program

---
+++ Chapter-05/Problem-02.py
@@ -0,0 +1,14 @@
+# Write a program to input eight numbers from the user and display all the unique numbers (once). 
+
+unique_numbers = set()
+
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+unique_numbers.add(int(input("Enter a number: ")))
+
+print("Unique numbers are:", unique_numbers)
