71c61e704cec40bf74959e2efcb828c5530c8d56
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-19 18:57:39 +0530

    Add Chapter 05: Introduction to Sets

---
+++ Chapter-05/03-Sets.py
@@ -0,0 +1,5 @@
+s = set()  # empty set
+print("Empty set:", s)  # Output: Empty set: set()
+
+e = {2, 3, 5, 48, 23, 2, 2}
+print("Set with elements:", e)  # Output: Set with elements: {2, 3, 5, 48, 23}
