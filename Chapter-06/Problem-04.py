ba024e79b75c3612dc4d87ddcee3e4c83cf293f0
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-23 22:55:23 +0530

    Add Chapter 06: Problem 04 - username length validation

---
+++ Chapter-06/Problem-04.py
@@ -0,0 +1,8 @@
+# 4. Write a program to find whether a given username contains less than 10 characters or not. 
+
+username = "anshul_padiyar"
+
+if (len(username)>9):
+    print("Invalid username, username contains more than 10 characters")
+else :
+    print("username:", username)
