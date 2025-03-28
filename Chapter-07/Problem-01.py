92767a0801ee7a28be34a24a71e65e4c36cff4b9
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-28 21:09:32 +0530

    Add Chapter 07: Problem 01 - multiplication table

---
+++ Chapter-07/Problem-01.py
@@ -0,0 +1,10 @@
+# 1. Write a program to print multiplication table of a given number using for loop. 
+
+num = int(input("Enter a number: "))
+
+i = 1
+
+print(f"Multiplication table for {num}:")
+while(i <= 10) :
+    print(f"{num} x {i} = ", (num*i))
+    i+=1
