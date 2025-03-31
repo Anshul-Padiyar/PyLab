284a81be7bd85094a10a5948336d4a7f3029dee4
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-03-31 18:24:04 +0530

    Update Chapter 07: Problem 02 greet names starting with 'B'

--- Chapter-07/Problem-02.py
+++ Chapter-07/Problem-02.py
@@ -1,7 +1,8 @@
-# 2. Write a program to greet all the person names stored in a list 'names' and which starts with S.
+# 2. Write a program to greet all the person names stored in a list 'names' and which starts with B.
 
 names = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
 print(f"List of names: {names}")  # Output: List of names: ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot']
 
 for name in names:
-    print(f"Hello {name}, What's up!")
+    if (name.startswith("B")): 
+        print(f"Hello {name}, What's up!") 
