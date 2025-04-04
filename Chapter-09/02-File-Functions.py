9f79cb949f2eb7314de487854d2c095e3b69ba2c
Anshul-Padiyar <anshulpadiyar2002@gmail.com>
2025-04-04 18:19:29 +0530

    Add example for reading files using readlines() and readline() methods

---
+++ Chapter-09/02-File-Functions.py
@@ -0,0 +1,14 @@
+file = open("file.txt")
+data = file.readlines() 
+# readlines(): Returns a list of lines from the file
+print(f"data:{data}\ntype of 'data': {type(data)}")
+file.close()
+
+file = open("file.txt")
+line01 = file.readline()
+# readline(): read the first line of the file "file.txt"
+print(line01)
+line02 = file.readline()
+# Call readline() twice to return both the first and the second line
+print(line02)
+file.close()
